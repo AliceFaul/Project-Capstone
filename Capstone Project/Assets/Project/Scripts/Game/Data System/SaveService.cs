f0a54d43d6c37627312ef6dd43c957e846b3d186
33Bruh <maihieu6714@gmail.com>
2026-09-11 11:06:46 +0700

    add: save service and step to startup

---
+++ Capstone Project/Assets/Project/Scripts/Game/Data System/SaveService.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class SaveService : IGameService
+{
+    private const string SaveKey = "PLAYER_DATA";
+    
+    private readonly ConfigManager _configManager;
+    private readonly PlayFabServiceManager _playFabService;
+    private readonly JsonWriter _writer;
+
+    private PlayFabDataFlow _dataService;
+    private PlayerDataConfig _config;
+
+    public SaveService(ConfigManager configManager, PlayFabServiceManager playFabService)
+    {
+        _configManager = configManager;
+        _playFabService = playFabService;
+        _writer = new JsonWriter(Application.persistentDataPath);
+    }
+    
+    public async Task<bool> Initialize(IServiceRegistry serviceRegistry, CancellationToken ct = default)
+    {
+        serviceRegistry.Register<SaveService>(this);
+        
+        if (!_configManager.GetConfig(out PlayerDataConfig config))
+        {
+            Debug.LogError($"[SaveService] Not found player data config in Config Manager.");
+            return false;
+        }
+        _config = config;
+
+        if (!_playFabService.TryGetService(out PlayFabDataFlow dataService))
+        {
+            Debug.LogError($"[SaveService] Not found data service in PlayFab services list.");
+            return false;
+        }
+        _dataService = dataService;
+        
+        await LoadData(ct);
+        return true;
+    }
+
+    private async Task LoadData(CancellationToken ct = default)
+    {
+        string cloudJson = null;
+
+        try
+        {
+            cloudJson = await _dataService.Load(SaveKey, ct);
+        }
+        catch (OperationCanceledException)
+        {
+            Debug.LogWarning($"[SaveService] Loading data timeout/canceled. Use local data.");
+        }
+
+        if (!string.IsNullOrEmpty(cloudJson))
+        {
+            try
+            {
+                var gameData = JsonUtility.FromJson<GameData>(cloudJson);
+                _config.ApplyGameData(gameData);
+                Debug.Log($"[SaveService] Charged data from PlayFab (cloud) successfully.)");
+                WriteLocalCache(cloudJson);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError($"[SaveService] Parse cloud data failed: {e.Message}");
+            }
+        }
+        else
+        {
+            Debug.Log($"[SaveService] Not found data in cloud. Using local data/default.");
+        }
+    }
+
+    public async Task<bool> SaveData(CancellationToken ct = default)
+    {
+        if(_config == null || _dataService == null) return false;
+        string json = JsonUtility.ToJson(_config.ToGameData());
+        WriteLocalCache(json);
+
+        bool cloudOk = false;
+        try
+        {
+            cloudOk = await _dataService.Save(SaveKey, json, ct);
+        }
+        catch (OperationCanceledException)
+        {
+            Debug.LogWarning($"[SaveService] Saving data timeout/canceled. Use local data.");
+        }
+        
+        if(!cloudOk) Debug.LogError($"[SaveService] Saving in cloud failed.");
+        return cloudOk;
+    }
+
+    private void WriteLocalCache(string json)
+    {
+        try
+        {
+            _writer.Write(json, _config.name + ".json");
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"[SaveService] Failed to write local cache. Error: {e.Message}]");
+        }
+    }
+}
---
+++ Capstone Project/Assets/Project/Scripts/Game/Data System/Startup/Steps/SaveStep.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SaveStep", menuName = "Startup/SaveStep")]
+public class SaveStep : StartupStep
+{
+    public override bool HasTimeout => true;
+    public override bool RequiresNetwork => false;
+
+    public override async Task<StartupStepResult> RunTasks(IServiceRegistry serviceRegistry, CancellationToken ct)
+    {
+        if (!serviceRegistry.TryGet<ConfigManager>(out var configMg))
+        {
+            Debug.LogError($"[SaveStep] Not found Config Manager - Please run config step first.");
+            return StartupStepResult.Failure("CONFIG_MANAGER_NOT_FOUND", $"Save step require Config Manager to run first.");
+        }
+
+        if (!serviceRegistry.TryGet<PlayFabServiceManager>(out var playFabMg))
+        {
+            Debug.LogError($"[SaveStep] Not found PlayFab Service Manager - Please run PlayFab step first.");
+            return StartupStepResult.Failure("PLAY_FAB_MANAGER_NOT_FOUND", $"Save step require PlayFab service manager to run first.");
+        }
+
+        var saveService = new SaveService(configMg, playFabMg);
+        try
+        {
+            bool initService = await saveService.Initialize(serviceRegistry, ct);
+            if (!initService)
+            {
+                Debug.LogError($"[SaveStep] Failed to initialize save service.");
+                return StartupStepResult.Failure("SAVE_INIT_FAILED", $"Save service failed to initialize.");
+            }
+        }
+        catch (OperationCanceledException)
+        {
+            Debug.LogWarning($"[SaveStep] Initialize timeout or cancelled");
+            return StartupStepResult.Failure("SAVE_INIT_TIMEOUT", $"Save service initialization timed out.");
+        }
+        catch (Exception e)
+        {
+            Debug.LogException(e);
+            return StartupStepResult.Failure("SAVE_INIT_EXCEPTION",  $"Exception while initializing Save service: {e.Message}");
+        }
+
+        return StartupStepResult.Success();
+    }
+}
